90981e8f3f103a78a2982a4f1f374f8a93c0d933
julian87nicolas <julicmrgo@gmail.com>
2022-06-03 15:34:58 -0300

    fixed duplicated ids

--- src/components/Bottom.js
+++ src/components/Bottom.js
@@ -9,10 +9,10 @@ function Bottom (props) {
             
             <div className='total'>
                 <div className='title'>Total this month</div>
-                <div id='value'>${props.total}</div>
+                <div id='total-value'>${props.total}</div>
             </div>
             <div className='aditional-info'>
-                <div id='value'>+2.4%</div>
+                <div id='aditional-info-value'>+2.4%</div>
                 <div className='title'>from last month</div>
             </div>
         </div>
